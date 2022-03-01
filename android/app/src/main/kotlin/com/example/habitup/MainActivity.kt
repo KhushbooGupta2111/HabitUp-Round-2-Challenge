8819871b74056a239ad2fe68796777d9a70384c3
Khushboo <guptakhushboo81537@gmail.com>
2022-03-01 09:45:42 +0530

    Initialised project

---
+++ android/app/src/main/kotlin/com/example/habitup/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.habitup
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/material.dart';
+import 'package:easy_sidemenu/easy_sidemenu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  static const color = const Color(0xffb74093);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Habit Up'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  PageController page = PageController();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SideMenu(
+            controller: page,
+            style: SideMenuStyle(
+              displayMode: SideMenuDisplayMode.auto,
+              hoverColor: Colors.blue[100],
+              selectedColor: Colors.lightBlue,
+              selectedTitleTextStyle: TextStyle(color: Colors.white),
+              selectedIconColor: Colors.white,
+              // backgroundColor: Colors.amber
+              // openSideMenuWidth: 200
+            ),
+            title: Column(
+              children: [
+                ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxHeight: 150,
+                    maxWidth: 150,
+                  ),
+                  child: Image.asset(
+                    'assets/images/easy_sidemenu.png',
+                  ),
+                ),
+                Divider(
+                  indent: 8.0,
+                  endIndent: 8.0,
+                ),
+              ],
+            ),
+            footer: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'mohada',
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+            items: [
+              SideMenuItem(
+                priority: 0,
+                title: 'Dashboard',
+                onTap: () {
+                  page.jumpToPage(0);
+                },
+                icon: Icons.home,
+              ),
+              SideMenuItem(
+                priority: 1,
+                title: 'Users',
+                onTap: () {
+                  page.jumpToPage(1);
+                },
+                icon: Icons.supervisor_account,
+              ),
+              SideMenuItem(
+                priority: 2,
+                title: 'Files',
+                onTap: () {
+                  page.jumpToPage(2);
+                },
+                icon: Icons.file_copy_rounded,
+              ),
+              SideMenuItem(
+                priority: 3,
+                title: 'Download',
+                onTap: () {
+                  page.jumpToPage(3);
+                },
+                icon: Icons.download,
+              ),
+              SideMenuItem(
+                priority: 4,
+                title: 'Settings',
+                onTap: () {
+                  page.jumpToPage(4);
+                },
+                icon: Icons.settings,
+              ),
+              SideMenuItem(
+                priority: 6,
+                title: 'Exit',
+                onTap: () async {},
+                icon: Icons.exit_to_app,
+              ),
+            ],
+          ),
+          Expanded(
+            child: PageView(
+              controller: page,
+              children: [
+                Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: Text(
+                      'Page\n   1',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: Text(
+                      'Page\n   2',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: Text(
+                      'Page\n   3',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: Text(
+                      'Page\n   4',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: Text(
+                      'Page\n   5',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         // Here we take the value from the MyHomePage object that was created by
+//         // the App.build method, and use it to set our appbar title.
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
